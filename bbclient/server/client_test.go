3ce3a0660cdfc3c1be70e7b9b3ba1808c5b8bcf2
Peter Zandbergen <peter.zandbergen@myhops.com>
2024-11-04 13:26:07 +0100

    Add env var to test

--- bbclient/server/client_test.go
+++ bbclient/server/client_test.go
@@ -2,19 +2,20 @@ package server
 
 import (
 	"context"
+	"os"
 	"testing"
 
 	"github.com/myhops/bbfs/nulllog"
 )
 
-const (
-	accessKey = ""
-)
+func getAccessKey() string {
+	return os.Getenv("BBFSSRV_ACCESS_KEY")
+}
 
 func TestGetReadme(t *testing.T) {
 	c := &Client{
 		BaseURL:   "https://bitbucket.belastingdienst.nl/rest/api/latest",
-		AccessKey: accessKey,
+		AccessKey: SecretString(getAccessKey()),
 		Logger: nulllog.Logger(),
 	}
 	content, err := c.GetFileContent(context.Background(), &GetFileContentCommand{
@@ -32,7 +33,7 @@ func TestGetReadme(t *testing.T) {
 func TestGetTags(t *testing.T) {
 	c := &Client{
 		BaseURL:   "https://bitbucket.belastingdienst.nl/rest/api/latest",
-		AccessKey: accessKey,
+		AccessKey: SecretString(getAccessKey()),
 	}
 	tags, err := c.GetTags(context.Background(), &GetTagsCommand{
 		ProjectKey: "~zandp06",
@@ -48,7 +49,7 @@ func TestGetTags(t *testing.T) {
 func TestGetFiles(t *testing.T) {
 	c := &Client{
 		BaseURL:   "https://bitbucket.belastingdienst.nl/rest/api/latest",
-		AccessKey: accessKey,
+		AccessKey: SecretString(getAccessKey()),
 	}
 	files, err := c.GetFiles(context.Background(), &GetFilesCommand{
 		ProjectKey: "~zandp06",
@@ -65,7 +66,7 @@ func TestGetFiles(t *testing.T) {
 func TestGetAllFiles(t *testing.T) {
 	c := &Client{
 		BaseURL:   "https://bitbucket.belastingdienst.nl/rest/api/latest",
-		AccessKey: accessKey,
+		AccessKey: SecretString(getAccessKey()),
 	}
 	files, err := c.GetFiles(context.Background(), &GetFilesCommand{
 		ProjectKey: "~zandp06",
@@ -82,7 +83,7 @@ func TestGetAllFiles(t *testing.T) {
 func TestIterator(t *testing.T) {
 	c := &Client{
 		BaseURL:   "https://bitbucket.belastingdienst.nl/rest/api/latest",
-		AccessKey: accessKey,
+		AccessKey: SecretString(getAccessKey()),
 	}
 	iter, err := c.GetFilesIterator(context.Background(), &GetFilesCommand{
 		ProjectKey: "~zandp06",
